075197913300acf66f7d58b9f8e463443c9a2890
Karina Yeznaian <karinayeznaian@yahoo.com>
2017-06-21 18:11:09 -0700

    dependency injection working

--- backslash.API/Controllers/FileApiController.cs
+++ backslash.API/Controllers/FileApiController.cs
@@ -2,12 +2,9 @@
 using Backslash.Data.Helpers;
 using Backslash.Service.Interfaces;
 using Backslash.Web.Models.Responses;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 
@@ -51,40 +48,17 @@ namespace backslash.API.Controllers
             // Check if the request contains multipart/form-data.
             if (!Request.Content.IsMimeMultipartContent())
             {
-                //throw new ErrorResponse(errMsg);
-                //replace with ErrorResponse class, pass an error string
                 throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
             }
 
 
-            //Request: info from the ajax call
             InMemoryMultipartStreamProviderService provider = await content.ReadAsMultipartAsync(new InMemoryMultipartStreamProviderService());
 
-
-            //if (provider.FormData["fileTags"] == null)
-            //{
-            //    throw new HttpResponseException(HttpStatusCode.BadRequest);
-            //}
-
-            //if (provider.FormData["fileTags"] != null)
-            //{
-            //    model = provider.FormData["fileTags"];
-            //    tags = JsonConvert.DeserializeObject<List<Tag>>(model);
-            //}
 
             if (provider.FormData["base64Image"] == null)
                 throw new HttpResponseException(HttpStatusCode.BadRequest);
 
-            //var base64Image = provider.FormData["base64Image"];
-
-            // The await operator suspends Upload().
-            //  - Upload() can't continue until Upload(provider), which is in FilesService, is complete.
-            //  - Meanwhile, control returns to the caller of Upload().
-            //  - Control resumes here when Upload(provider) is complete. 
-            //  - The await operator then retrieves the string result from Upload(provider).
-
             ItemResponse<bool> response = new ItemResponse<bool>();
-
 
             response.Item = await _fileService.AddFile(provider, directory, userId);
 
